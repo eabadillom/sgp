f1d30d2aacdfb1a77087a0309bc51f295fac8ea8
calberto <alberto@alberto-System-Product-Name>
2024-10-02 09:31:54 -0600

    Implementacion sección ISR y Subsidio

--- src/main/java/mx/com/ferbo/dao/n/SubsidioDAO.java
+++ src/main/java/mx/com/ferbo/dao/n/SubsidioDAO.java
@@ -14,32 +14,60 @@ import mx.com.ferbo.commons.dao.BaseDAO;
 import mx.com.ferbo.model.CatSubsidio;
 
 public class SubsidioDAO extends BaseDAO<CatSubsidio, Integer> {
-	
-	private static Logger log = LogManager.getLogger(SubsidioDAO.class);
 
-	public SubsidioDAO(Class<CatSubsidio> modelClass) {
-		super(modelClass);
-	}
-	
-	public List<CatSubsidio> buscar(Date fechaInicio, Date fechaFin, String periodo) {
-    	List<CatSubsidio> modelList = null;
-    	EntityManager emSGP = null;
-    	
-    	try {
-    		emSGP = this.getEntityManager();
-    		modelList = emSGP.createNamedQuery("CatSubsidio.findByPeriodoTipo", modelClass)
-    				.setParameter("fechaInicio", fechaInicio)
-    				.setParameter("fechaFin", fechaFin)
-    				.setParameter("periodo", periodo)
-    				.getResultList()
-    				;
-    	} catch(NoResultException ex) {
-    		log.error("No se encontró la tabla de subsidio solicitada...", ex);
-    		modelList = new ArrayList<>();
-    	} finally {
-    		this.close(emSGP);
-    	}
-    	
-    	return modelList;
+    private static Logger log = LogManager.getLogger(SubsidioDAO.class);
+
+    public SubsidioDAO(Class<CatSubsidio> modelClass) {
+        super(modelClass);
     }
+    
+    public SubsidioDAO()
+    {
+        super(CatSubsidio.class);
+    }
+
+    public List<CatSubsidio> buscar(Date fechaInicio, Date fechaFin, String periodo) {
+        List<CatSubsidio> modelList = null;
+        EntityManager emSGP = null;
+
+        try {
+            emSGP = this.getEntityManager();
+            modelList = emSGP.createNamedQuery("CatSubsidio.findByPeriodoTipo", modelClass)
+                    .setParameter("fechaInicio", fechaInicio)
+                    .setParameter("fechaFin", fechaFin)
+                    .setParameter("periodo", periodo)
+                    .getResultList();
+        } catch (NoResultException ex) {
+            log.error("No se encontró la tabla de subsidio solicitada...", ex);
+            modelList = new ArrayList<>();
+        } finally {
+            this.close(emSGP);
+        }
+
+        return modelList;
+    }
+    
+    public List<CatSubsidio> listaSubsidio(Date fecha)
+    {
+        List<CatSubsidio> modelList = null;
+        EntityManager em = null;
+        
+        try
+        {
+            em = this.getEntityManager();
+            modelList = em.createNamedQuery("CatSubsidio.findAllBeforeDate", CatSubsidio.class)
+                .setParameter("fecha", fecha)
+                .getResultList();
+        }catch(NoResultException ex)
+        {
+            log.error("No se encontró la tabla de subsidio solicitada...", ex);
+            modelList = new ArrayList<>();
+        }finally
+        {
+            this.close(em);
+        }
+        
+        return modelList;
+    }
+    
 }
